7e61b1bc2b008340a0139b8af6a308083cad46d6
SandSurfer <andyn5068@gmail.com>
2023-03-05 22:42:57 -0800

    Initial loading of libraries

---
+++ Graphics.R
@@ -0,0 +1,8 @@
+## REMEMBER TO INSTALL PACKAGES AND RUN PACKAGES
+library("tidyverse") # Used for coding style
+library("afex")      # Perform stats (ANOVAs)
+library("emmeans")   # Calculates estimated marginal means
+library("writexl")   # Produces Excel files from R data
+library("readxl")    # Load Excel files into R
+library("ggridges")  # Add ridge plots.
+library("wesanderson")
---
+++ Library.R
@@ -0,0 +1,10 @@
+# Libraries
+
+install.packages("tidyverse") # Used for coding style
+install.packages("afex")      # Perform stats (ANOVAs)
+install.packages("emmeans")   # Calculates estimated marginal means
+install.packages("writexl")   # Produces Excel files from R data
+install.packages("readxl")    # Load Excel files into R
+install.packages("ggthemes")  # Graphing aesthetics
+install.packages("ggridges")  # Add ridge plots.
+install.packages("wesanderson") # WesAnderson Themed Plots
